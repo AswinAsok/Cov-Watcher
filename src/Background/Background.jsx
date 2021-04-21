f660d46c8030f948f88636a533ae480d2f8e3ea7
Aswin Asok <aswinasokofficial@gmail.com>
2021-04-21 18:35:39 +0530

    shadow added broder color changed

--- src/Background/Background.jsx
+++ src/Background/Background.jsx
@@ -5,7 +5,7 @@ import "./Background.css";
 
 const defaultProps = {
   bgcolor: "background.paper",
-  borderColor: "text.primary",
+  borderColor: "#C0C0C0",
   m: 1,
   border: 1,
   style: { width: "95%", height: "40rem", marginTop: "3rem", marginBottom: "3rem"},
@@ -15,7 +15,7 @@ const Background = () => {
   return (
     <div display="flex" justifyContent="center" className="main-container">
       <Grid container direction="row" justify="center" alignItems="center">
-        <Box borderRadius={16} {...defaultProps} />
+        <Box boxShadow={3} borderRadius={16} {...defaultProps} />
       </Grid>
     </div>
   );
