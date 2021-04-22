66e5c15ed0fdd9d229a6592b4b6a678c11be7c97
Aswin Asok <aswinasokofficial@gmail.com>
2021-04-22 20:02:29 +0530

    Data which is being fetched from the api is stored state wise and is displayed onto displaystat.

--- src/Displaystat/Displaystat.jsx
+++ src/Displaystat/Displaystat.jsx
@@ -1,27 +1,59 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from '@material-ui/core/Box';
 
-const Displaystat = ({data, statecode}) => {
+const defaultProps = {
+    bgcolor: 'background.paper',
+    borderColor: 'text.primary',
+    m: 1,
+    border: 1,
+    style: { width: '5rem', height: '5rem' },
+  };
 
-    const [statedata, setStatedata] = useState({empty: "yes"})
+const Displaystat = ({ data, statecode }) => {
+  const classes = useStyles();
+  const [statedata, setStatedata] = useState({ empty: "yes" });
 
-    const fetchstatedata = () => {
-        if(statedata.empty === "yes" && data.empty !== "yes"){
-            for(var i = 0 ; i < 38 ; i++){
-                if(data.statewise[i].statecode === statecode){
-                    setStatedata(data.statewise[i])
-                    break;
-                }
-            }
+  const fetchstatedata = () => {
+    if (statedata.empty === "yes" && data.empty !== "yes") {
+      for (var i = 0; i < 38; i++) {
+        if (data.statewise[i].statecode === statecode) {
+          setStatedata(data.statewise[i]);
+          break;
         }
+      }
     }
+  };
 
-    return (
+  return (
+    <div>
+      {fetchstatedata()}
+      <Grid
+        container
+        direction="row"
+        justify="space-evenly"
+        alignItems="center"
+        spacing={3}
+      >
         <div>
-            <p>State Code: {statecode}</p>
-            {fetchstatedata()}
-            {console.log(statedata)}
+          <Grid item xs>
+            <p>{statedata.active}</p>
+          </Grid>
         </div>
-    )
-}
+        <div>
+          <Grid item xs>
+            <p>{statedata.confirmed}</p>
+          </Grid>
+        </div>
+        <div>
+          <Grid item xs>
+            <p>{statedata.deaths}</p>
+          </Grid>
+        </div>
+      </Grid>
+    </div>
+  );
+};
 
-export default Displaystat
+export default Displaystat;
