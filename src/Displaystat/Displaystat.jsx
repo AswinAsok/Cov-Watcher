af22965dbae4809a94c765d580caccc30aa4466c
Aswin Asok <aswinasokofficial@gmail.com>
2021-04-22 19:08:17 +0530

    Statewise data is getting stored to the state variable of the Displaystat component

--- src/Displaystat/Displaystat.jsx
+++ src/Displaystat/Displaystat.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Displaystat = ({data, statecode}) => {
+
+    const [statedata, setStatedata] = useState({empty: "yes"})
+
+    const fetchstatedata = () => {
+        if(statedata.empty === "yes" && data.empty !== "yes"){
+            for(var i = 0 ; i < 38 ; i++){
+                if(data.statewise[i].statecode === statecode){
+                    setStatedata(data.statewise[i])
+                    break;
+                }
+            }
+        }
+    }
+
     return (
         <div>
             <p>State Code: {statecode}</p>
+            {fetchstatedata()}
+            {console.log(statedata)}
         </div>
     )
 }
