e80b6eae36ca687f1b172a91a50343b4a99c313b
Aswin Asok <aswinasokofficial@gmail.com>
2021-04-23 06:45:49 +0530

    Rem => vh and vw

--- src/Displaystat/Displaystat.jsx
+++ src/Displaystat/Displaystat.jsx
@@ -6,12 +6,7 @@ import "./Displaystat.css";
 
 const defaultProps = {
   m: 1,
-  style: { width: "150px", height: "3rem" },
-};
-
-const defaultProps1 = {
-  m: 1,
-  style: { width: "115px", height: "2rem" },
+  style: { width: "25vw", height: "6.5vh" },
 };
 
 const Displaystat = ({ data, statecode }) => {
@@ -32,49 +27,44 @@ const Displaystat = ({ data, statecode }) => {
     <div>
       {fetchstatedata()}
       <br></br>
-      
+
       <Grid container direction="row" justify="center" alignItems="center">
-        <div>
-          <Box className="statrow1" borderRadius={9} {...defaultProps}>
-            <div className="stattextr1">Active</div>
-            <div className="stattextr1">{statedata.active}</div>
-          </Box>
-        </div>
-        <div>
-          <Box className="statrow2" borderRadius={9} {...defaultProps}>
-            <div className="stattextr1">Confirmed</div>
-            <div className="stattextr1">{statedata.confirmed}</div>
-          </Box>
-        </div>
-        <div>
-          <Box className="statrow3" borderRadius={9} {...defaultProps}>
-            <div className="stattextr1">Deaths</div>
-            <div className="stattextr1">{statedata.deaths}</div>
-          </Box>
-        </div>
+        
+        <Box className="statrow1" borderRadius={9} {...defaultProps}>
+          <div className="stattextr11">Active</div>
+          <div className="stattextr1">{statedata.active}</div>
+        </Box>
+
+        <Box className="statrow2" borderRadius={9} {...defaultProps}>
+          <div className="stattextr11">Confirmed</div>
+          <div className="stattextr1">{statedata.confirmed}</div>
+        </Box>
+
+        <Box className="statrow3" borderRadius={9} {...defaultProps}>
+          <div className="stattextr11">Deaths</div>
+          <div className="stattextr1">{statedata.deaths}</div>
+        </Box>
       </Grid>
 
       <br></br>
 
       <Grid container direction="row" justify="center" alignItems="center">
-        <div>
-            <Box className="statrow11" borderRadius={9} {...defaultProps}>
-              <div className="stattextr1">New Cases</div>
-              <div className="stattextr1">{statedata.deltaconfirmed}</div>
-            </Box>
-        </div>
-        <div>
-            <Box className="statrow12" borderRadius={9} {...defaultProps}>
-              <div className="stattextr1">Newly Recovered</div>
-              <div className="stattextr1">{statedata.deltarecovered}</div>
-            </Box>
-        </div>
-        <div>
-            <Box className="statrow13" borderRadius={9} {...defaultProps}>
-              <div className="stattextr1">New Deaths</div>
-              <div className="stattextr1">{statedata.deltadeaths}</div>
-            </Box>
-        </div>
+
+          <Box className="statrow11" borderRadius={9} {...defaultProps}>
+            <div className="stattextr11">New Cases</div>
+            <div className="stattextr1">{statedata.deltaconfirmed}</div>
+          </Box>
+
+          <Box className="statrow12" borderRadius={9} {...defaultProps}>
+            <div className="stattextr11">Recovered</div>
+            <div className="stattextr1">{statedata.deltarecovered}</div>
+          </Box>
+
+          <Box className="statrow13" borderRadius={9} {...defaultProps}>
+            <div className="stattextr11">New Deaths</div>
+            <div className="stattextr1">{statedata.deltadeaths}</div>
+          </Box>
+
       </Grid>
     </div>
   );
